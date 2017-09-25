27ddeed2953c7d3714f788bd27532fc391198b43
Fred-Fan <xuancheng.fan@berkeley.edu>
2017-09-25 01:06:02 -0700

    code update
    1. create new script to get correct label

--- conversion/getlabels.py
+++ conversion/getlabels.py
@@ -1,16 +1,33 @@
 import glob
 import pandas as pd
 import re
+from collections import Counter
+import os
 
 label_list = []
-for f in glob.glob("kaggle\*.*"):
+id_list = []
+for f in glob.glob("D:\\Downloads\\kaggle\\*.*"):
     try:
-        file_id = re.findall(r'No\.(\d+)', f)[0]
+        head, tail = os.path.split(f)
+        file_id = int(re.findall(r'\d+', tail)[0]) - 1
         emotion = re.findall(r'\-(\w+)\-', f)[0]
         label_list.append([file_id, emotion])
+        id_list.append(file_id)
     except:
         print(f)
 
-label_pd = pd.DataFrame(label_list)
+# show missed id
+true_set = set(list(range(min(id_list), max(id_list)+1)))
+actual_set = set(id_list)
+print('missed', true_set - actual_set)
+
+# show duplicated id and remove
+duplicate_list = [item for item, count in Counter(id_list).items() if count > 1]
+print('duplicated', duplicate_list)
+
+label_pd = pd.DataFrame(label_list, columns = ['id', 'emotion'])
+
+for i in duplicate_list:
+    label_pd = label_pd[label_pd['id'] != i]
 
 label_pd.to_csv('correct_label.csv')
