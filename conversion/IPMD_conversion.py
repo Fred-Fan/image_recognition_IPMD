27c334aa7c75b2f6bc9708877adcdd4cdf9da4f3
Fred-Fan <xuancheng.fan@berkeley.edu>
2017-11-12 15:46:17 -0800

    code update

--- conversion/IPMD_conversion.py
+++ conversion/IPMD_conversion.py
@@ -11,62 +11,145 @@ import glob
 import os
 import pandas as pd
 import re
+import pickle
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+
+def firstmodify(left, right, up, bottom):
+    if (right-left)>=(bottom-up):
+        diff = (right-left)-(bottom-up)
+        if diff%2 == 0:
+            left = int(left-10)
+            right = int(right+10)
+            up = int(up-10-diff/2)
+            bottom = int(bottom+10+diff/2)
+
+        else:
+            left = int(left-10)
+            right = int(right+10)
+            up = int(up-10-(diff/2+0.5))
+            bottom = int(bottom+10+(diff/2-0.5))
+    else:
+        diff = (bottom-up)-(right-left)
+        if diff%2 == 0:
+            left = int(left-10-diff/2)
+            right = int(right+10+diff/2)
+            up = int(up-10)
+            bottom = int(bottom+10)
+        else:
+            left = int(left-10-(diff/2+0.5))
+            right = int(right+10+(diff/2-0.5))
+            up = int(up-10)
+            bottom = int(bottom+10)
+
+    return left, right, up, bottom
+
+
+def ifoverborder(left, right, up, bottom, width, height):
+    if left < 0:
+        right = right + (0-left)
+        left = 0
+        if right > width:
+            right = width
+    if right > width:
+        left = left - (right-width)
+        right = width
+        if left < 0:
+            left = 0
+    if up < 0:
+        bottom = bottom + (0-up)
+        up = 0
+        if bottom > height:
+            bottom = height
+    if bottom > height:
+        up = up - (bottom - height)
+        bottom = height
+        if up < 0:
+            up = 0
+    #print(left, right, up, bottom, width, height)
+    return left, right, up, bottom
+
+def finalmodify(left, right, up, bottom):
+    if right - left > bottom - up:
+        diff = (right-left)-(bottom-up)
+        if diff%2 == 0:
+            up = int(up+diff/2)
+            bottom = int(bottom-diff/2)
+        else:
+            up = int(up+(diff/2-0.5))
+            bottom = int(bottom-(diff/2-0.5))
+    else:
+        diff = (bottom-up)-(right-left)
+        if diff%2 == 0:
+            left = int(left+diff/2)
+            right = int(right-diff/2)
+        else:
+            left = int(left+(diff/2+0.5))
+            right = int(right-(diff/2+0.5))
+    return left, right, up, bottom
+
 
 
 def conversion(f):
     head, tail = os.path.split(f)
-    detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
     # load the input image, resize it, and convert it to grayscale
     image = cv2.imread(f)
-    image = imutils.resize(image, width=500)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # detect faces in the grayscale image
-    rects = detector(gray, 1)
-    for (i, rect) in enumerate(rects):
-        # determine the facial landmarks for the face region, then
-        # convert the landmark (x, y)-coordinates to a NumPy array
-        shape = predictor(gray, rect)
-        shape = face_utils.shape_to_np(shape)
-        left = []
-        right = []
-        up = []
-        bottom = []
+    height, width = image.shape[:2]
+    for new_width in range(width, 100, -10):
+        image = imutils.resize(image, width=new_width)
+    
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+        # detect faces in the grayscale image
+        rects = detector(gray, 1)
         for (i, rect) in enumerate(rects):
             # determine the facial landmarks for the face region, then
             # convert the landmark (x, y)-coordinates to a NumPy array
-            print(2)
-            shape = predictor(gray, rect)
-            shape = face_utils.shape_to_np(shape)
-            for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
-                (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
-                left.append(x)
-                right.append(x+w)
-                up.append(y)
-                bottom.append(y+h)
-            left_choose = min(left)
-            right_choose = max(right)
-            up_choose = min(up)
-            bottom_choose = max(bottom)
-            if (right_choose-left_choose)>=(bottom_choose-up_choose): 
-                diff = round(((right_choose-left_choose)-(bottom_choose-up_choose))/2)
-                roi = image[int(up_choose-10-diff/2):int(bottom_choose+10+diff/2), int(left_choose-10):int( right_choose+10)]
-            else:
-                diff = round(((bottom_choose-up_choose) - (right_choose-left_choose))/2)
-
-                roi = image[int(up_choose-10):int(bottom_choose+10), int(left_choose-10-diff/2):int(right_choose+10+        diff/2)]
-            #print(x,x+w,y,y+h )
-            #roi = image[y:y + h, x:x + w]
-            roi = imutils.resize(roi, width=96)
-            output = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-            outfile = 'output/'+tail
-            cv2.imwrite(outfile,output)
-            output = output.flatten()
-            #print(len(output))
-            return output
-
+            try:
+                print(rect[0])
+                continue
+            except TypeError:
+                print(new_width)
+                shape = predictor(gray, rect)
+                shape = face_utils.shape_to_np(shape)
+                left_list = []
+                right_list = []
+                up_list = []
+                bottom_list = []
+                #for (i, rect) in enumerate(rects):
+                    # determine the facial landmarks for the face region, then
+                    # convert the landmark (x, y)-coordinates to a NumPy array
+                #shape = predictor(gray, rect)
+                #shape = face_utils.shape_to_np(shape)
+                for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+                    (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+                    left_list.append(x)
+                    right_list.append(x+w)
+                    up_list.append(y)
+                    bottom_list.append(y+h)
+                left = min(left_list)
+                right = max(right_list)
+                up = min(up_list)
+                bottom = max(bottom_list)
+                left, right, up, bottom = firstmodify(left, right, up, bottom)
+                left, right, up, bottom = ifoverborder(left, right, up, bottom, width, height)
+        
+                left, right, up, bottom = finalmodify(left, right, up, bottom)
+                #print(left, right, up, bottom)
+        
+                roi = image[up:bottom, left:right]
+                #roi = image[y:y + h, x:x + w]
+                roi = cv2.resize(roi, (96,96), interpolation = cv2.INTER_AREA)
+                output = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+                outfile = 'output/'+tail
+                cv2.imwrite(outfile,output)
+                output = output.flatten()
+                #print(len(output))
+                return output
+        
 
 
 def main():
@@ -74,7 +157,7 @@ def main():
 
     #pool = Pool(processes=process_number)
     output_array = []
-    for f in glob.glob('images/*.*'):
+    for f in glob.glob('all/*.*'):
         print(f)
         head, tail = os.path.split(f)
         photo_id = re.findall(r'\d+', tail)[0]
@@ -88,8 +171,9 @@ def main():
             print('error')
     #pool.close()
     output_pd = pd.DataFrame(output_array, columns =['id', 'pixels', 'emotion'])
-    output_pd.to_csv('temp.csv')
-
+    #output_pd.to_csv('temp.csv')
+    with open('temp.pd', 'wb') as fout:
+        pickle.dump(output_pd, fout)
 
 if __name__ == "__main__":
     multiprocessing.freeze_support()  # must run for windows
