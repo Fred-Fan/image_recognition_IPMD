758d27611d9e474998cba3bbb42ae55ee5913813
Fred-Fan <xuancheng.fan@berkeley.edu>
2017-12-25 00:32:39 -0800

    code update
    1. check the image size
    2. handle strange emotions

--- conversion/getpixel.py
+++ conversion/getpixel.py
@@ -15,9 +15,15 @@ def main():
         image = cv2.imread(f)
         output = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         output = output.flatten()
+        if len(output) != 9216:
+            print(f)
         head, tail = os.path.split(f)
         photo_id = re.findall(r'\d+', tail)[0]
-        emotion = re.findall(r'\-(\w+)\-', tail)[0]
+        try:
+            emotion = re.findall(r'[ \-](\w+)', tail)[0]
+        except:
+            print(f)
+            emotion = ''
         output_array.append([photo_id, output, emotion, tail])
 
     output_pd = pd.DataFrame(output_array, columns =['id', 'pixels', 'emotion', 'original_file'])
