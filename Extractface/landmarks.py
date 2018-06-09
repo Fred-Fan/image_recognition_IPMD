f1f0186d208fdac7db56885e773147067e7ba84d
Fred-Fan <xuancheng.fan@berkeley.edu>
2018-06-09 16:48:43 -0700

    add margin

--- Extractface/landmarks.py
+++ Extractface/landmarks.py
@@ -17,34 +17,51 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 
-def firstmodify(left, right, up, bottom):
+def firstmodify(left, right, up, bottom, margin_perc=30):
+    """
+    This function is to make a square images based on the facemark + margin.
+
+    Args:
+        left(int): the left border of the image.
+        right(int): the right border of the image.
+        up(int): the bottom border of the image.
+        bottom(int): the upper border of the image.
+        margin_perc(int): the percentage, change it to control the margin width
+
+    Returns:
+        int: the new border of the image.
+
+    """
     if (right-left)>=(bottom-up):
+        margin = int((right-left)*margin_perc/100)
         diff = (right-left)-(bottom-up)
         if diff%2 == 0:
-            left = int(left-10)
-            right = int(right+10)
-            up = int(up-10-diff/2)
-            bottom = int(bottom+10+diff/2)
+            left = int(left-margin)
+            right = int(right+margin)
+            up = int(up-margin-diff)
+            bottom = int(bottom+margin)
 
         else:
-            left = int(left-10)
-            right = int(right+10)
-            up = int(up-10-(diff/2+0.5))
-            bottom = int(bottom+10+(diff/2-0.5))
+            left = int(left-margin)
+            right = int(right+margin)
+            up = int(up-margin-diff)
+            bottom = int(bottom+margin)
     else:
+        margin = int((bottom-up)*margin_perc/100)
         diff = (bottom-up)-(right-left)
         if diff%2 == 0:
-            left = int(left-10-diff/2)
-            right = int(right+10+diff/2)
-            up = int(up-10)
-            bottom = int(bottom+10)
+            left = int(left-margin-diff/2)
+            right = int(right+margin+diff/2)
+            up = int(up-margin)
+            bottom = int(bottom+margin)
         else:
-            left = int(left-10-(diff/2+0.5))
-            right = int(right+10+(diff/2-0.5))
-            up = int(up-10)
-            bottom = int(bottom+10)
+            left = int(left-margin-(diff/2+0.5))
+            right = int(right+margin+(diff/2-0.5))
+            up = int(up-margin)
+            bottom = int(bottom+margin)
 
     return left, right, up, bottom
+
 
 
 def ifoverborder(left, right, up, bottom, width, height):
